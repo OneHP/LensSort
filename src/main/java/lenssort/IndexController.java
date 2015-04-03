d981f41cf090abaa3b234f5159adbec61df20052
onehp <onehitpoint@googlemail.com>
2015-04-03 14:20:45 +0100

    Serve index page with Thymeleaf

---
+++ src/main/java/lenssort/IndexController.java
@@ -0,0 +1,15 @@
+package lenssort;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
