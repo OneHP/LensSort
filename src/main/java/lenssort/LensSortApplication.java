5511c5471f466dd89e85771a17a90de315a97d53
onehp <onehitpoint@googlemail.com>
2015-04-03 11:24:17 +0100

    Initial Commit
    Skeleton Spring Boot Application

---
+++ src/main/java/lenssort/LensSortApplication.java
@@ -0,0 +1,12 @@
+package lenssort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LensSortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LensSortApplication.class, args);
+    }
+}
